fb65eb5d5fd38ebb08bdb0542e5e38dd7efdf1a4
jikSAN <lime9churr@proton.me>
2023-01-24 10:45:09 +0530

    Created Homepage and blog index created posts table

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,31 @@
+<x-app-layout>
+	<div class="w-4/5 m-auto my-16 py-5 text-center">
+		<div class="border-b border-gray-200">
+			<h1 class="text-6xl">
+				Blog Posts
+			</h1>
+		</div>
+	</div>
+	<div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto my-14 pt-15 pb-24 border-b border-gray-200">
+		<div>
+			<img src="https://cdn.pixabay.com/photo/2015/01/08/18/25/desk-593327_960_720.jpg" alt="">
+		</div>
+		<div>
+			<h2 class="text-gray-700 font-bold text-4xl pb-4">
+				Learn how to write Laravel code
+			</h2>
+			<span class="text-gray-500">
+				By <span class="font-bold italic text-gray-800">jikSAN</span>,1 day ago
+			</span>
+			<p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</p>
+			<a href="" class="uppercase text-gray-100 bg-blue-500 text-lg font-extrabold py-4 px-8 rounded-2xl">
+				Keep Reading
+			</a>
+		</div>
+	</div>
+</x-app-layout>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,31 @@
+<div class="bg-gray-700">
+  <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+    <header class="flex justify-between items-center py-3 md:py-3">
+      <!-- logo - start -->
+      <a href="/" class="inline-flex items-center text-white text-2xl md:text-3xl font-bold gap-2.5" aria-label="logo">
+        <svg width="95" height="94" viewBox="0 0 95 94" class="w-6 h-auto text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path d="M96 0V47L48 94H0V47L48 0H96Z" />
+        </svg>
+
+        LaraBlog
+      </a>
+      <!-- logo - end -->
+
+      <!-- nav - start -->
+      <nav class="hidden lg:flex gap-12">
+        <a href="{{ route('index') }}" class="text-gray-200 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">Home</a>
+        <a href="/blog" class="text-gray-200 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">Blog</a>
+        <a href="#" class="text-gray-200 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">About</a>
+      </nav>
+      <!-- nav - end -->
+
+      <!-- buttons - start -->
+      <div class="hidden lg:flex flex-col sm:flex-row sm:justify-center lg:justify-start gap-2.5 -ml-8">
+        <a href="#" class="inline-block focus-visible:ring ring-indigo-300 text-gray-100 hover:text-indigo-500 active:text-indigo-600 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 py-3">Login</a>
+
+        <a href="#" class="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Register</a>
+      </div>
+      <!-- buttons - end -->
+    </header>
+  </div>
+</div>
